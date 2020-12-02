eb494267b960ccd66d34e94124e6825194ef38fe
Mariana Chang Ieong <mariana.chang-ieong@polymtl.ca>
2020-12-02 14:24:59 -0500

    MCI: ch7 review

--- ch7.py
+++ ch7.py
@@ -1,24 +1,24 @@
 """
 Exemple des notions du chapitre 7.
 """
-
-
 from chatbot import *
 from twitch_bot import *
 
-
-def build_say_hi_callback(bot, message):
+def build_say_hi_callback(bot: TwitchBot, message):
 	# TODO: Créer et retourner une fonction qui prend un paramètre (ignoré).
 	#       Cette fonction envoie `message` dans le chat à l'aide de la méthode `send_privmsg` du paramètre `bot`.
-	pass
+	def callback(*args):
+		bot.send_privmsg(message)
+
+	return callback
 
 def run_ch7_example():
 	bot = TwitchBot("logs")
 	# TODO: Construire le callback avec le bot et un message de votre choix.
-	callback = ...
+	callback = build_say_hi_callback(bot, "Test Message")
 	# TODO: Enregister le callback sous la commande "say_hi".
-	bot.register_command(...)
+	bot.register_command("say_hi", callback)
 	# TODO: Mettre votre jeton (incluant le "oauth:") et le nom du compte Twitch associé.
-	bot.connect_and_join("oauth:...", "...", "samsei109")
+	bot.connect_and_join("oauth:...", "la star", "marianachng")
 	bot.run()
 
